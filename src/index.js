e016d63cec921890d54bc0a8e48f0f16af99a34c
Katarina Kyrylenko <katarina.kyrylenko@gmail.com>
2021-01-18 23:18:26 +0200

    fin

--- src/index.js
+++ src/index.js
@@ -4,22 +4,37 @@ import refs from "./js/refs.js";
 import countriesList from "./templates/countries-list.hbs";
 import oneCountryInfo from "./templates/one-country.hbs";
 import debounce from "lodash/debounce";
+import { error } from "@pnotify/core/";
+import "@pnotify/core/dist/BrightTheme.css";
 
 console.log(fetchCountries);
 const { input, articles } = refs;
+
 input.addEventListener("input", debounce(onInputChange, 500));
 
 function onInputChange(e) {
+  e.preventDefault();
+  clearMarkup();
   //
-  fetchCountries(e.target.value).then((data) => {
-    console.log(data);
-    if (data.length === 1) {
-      buildCountryMarkup(articles, data);
-    }
-    if (data.length > 1) {
-      buildList(articles, data);
-    }
-  });
+  fetchCountries(e.target.value)
+    .then((data) => {
+      console.log(data);
+      if (data.length === 1) {
+        buildCountryMarkup(articles, data);
+      }
+      if (data.length > 1 && data.length < 10) {
+        buildList(articles, data);
+      }
+      if (data.length > 10) {
+        error({
+          text: "Too many matches found. Please enter a more specific query!",
+          delay: 2000,
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 function buildCountryMarkup(place, country) {
@@ -28,4 +43,8 @@ function buildCountryMarkup(place, country) {
 
 function buildList(place, countries) {
   place.insertAdjacentHTML("beforeend", countriesList(countries));
+}
+
+function clearMarkup() {
+  refs.articles.innerHTML = "";
 }
